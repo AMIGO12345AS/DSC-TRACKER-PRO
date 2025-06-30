a18235b7b89714980416b46fa4ae2fce4dedc7e2
ASEEM NRS WORKS <aseemnrs@gmail.com>
2025-06-30 12:00:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/manage-users-dialog.tsx
+++ src/components/manage-users-dialog.tsx
@@ -18,6 +18,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { Button } from './ui/button';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './ui/table';
