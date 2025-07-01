4d031da78c6bf1172f117123ff2a9ae8250044e4
ASEEM NRS WORKS <aseemnrs@gmail.com>
2025-07-01 06:58:41 +0000

    also isnt that looking a little off? there that blue thing which goes un

--- src/components/sub-box-modal.tsx
+++ src/components/sub-box-modal.tsx
@@ -121,7 +121,7 @@ export function SubBoxModal({ isOpen, onClose, mainBoxId, dscs, onDscSelect, log
                     <>
                         <h3 className="font-semibold mb-2">Sub-box {selectedSubBox.toUpperCase()}</h3>
                         <ScrollArea className="h-[240px] flex-grow pr-3">
-                            <div className="space-y-1 text-left">
+                            <div className="space-y-1 text-left py-1">
                                 {dscsInSelectedSubBox.map((dsc) => (
                                     <div
                                         key={dsc.id}
